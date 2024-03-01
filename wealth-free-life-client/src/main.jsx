4f706469a2baca1a34e57a6755d0b83b6788d943
iibrahim70 <aviknuhash70@gmail.com>
2024-03-01 20:49:54 +0600

    remove nfts and wallet

--- wealth-free-life-client/src/main.jsx
+++ wealth-free-life-client/src/main.jsx
@@ -1,7 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { WagmiProvider } from "wagmi";
-import { config } from "./config";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import AuthProvider from "./providers/AuthProvider";
 import { RouterProvider } from "react-router-dom";
@@ -12,12 +10,10 @@ const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <WagmiProvider config={config}>
-      <QueryClientProvider client={queryClient}>
-        <AuthProvider>
-          <RouterProvider router={router} />
-        </AuthProvider>
-      </QueryClientProvider>
-    </WagmiProvider>
+    <QueryClientProvider client={queryClient}>
+      <AuthProvider>
+        <RouterProvider router={router} />
+      </AuthProvider>
+    </QueryClientProvider>
   </React.StrictMode>
 );
--- wealth-free-life-client/src/utils/constants.js
+++
@@ -1,4 +0,0 @@
-import abi from "./Transactions.json";
-
-export const contractAbi = abi.abi;
-export const contractAddress = "0x818f52dd11926957b2125da374a9eecf65c1cc44";
