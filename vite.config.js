8ec2f5a4f9e692ce9574f7f643104f50c01217c4
RoknuzzamanRokon <rokon.raz@gmail.com>
2024-03-02 21:13:11 +0600

    Add target esnext

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
-    target: 'esmodule',
+    target: 'esnext',
   },
   plugins: [react()],
   resolve: {
