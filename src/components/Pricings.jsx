808288e7ca2bf9c4eca904a2800b84a4398e601f
iibrahim70 <aviknuhash70@gmail.com>
2024-03-28 14:39:59 +0600

    update pricings section

--- src/components/Pricings.jsx
+++ src/components/Pricings.jsx
@@ -1,44 +1,15 @@
+import { useGetAllPackagesQuery } from "@/redux/services/packageApi";
+import Skeleton from "react-loading-skeleton";
+
 const Pricings = () => {
-  const pricings = [
-    {
-      id: 1,
-      price: "$99/ Monthly",
-      title: "Basic Plan",
-      details: [
-        "Basic features included",
-        "Limited support",
-        "Access to community forums",
-        "Monthly newsletter subscription",
-        "Online documentation",
-      ],
-    },
-    {
-      id: 2,
-      price: "$299/ Monthly",
-      title: "Standard Plan",
-      details: [
-        "Advanced features included",
-        "Priority support",
-        "Access to premium content library",
-        "Quarterly webinar series",
-        "Phone support available during business hours",
-      ],
-    },
-    {
-      id: 3,
-      price: "$599/ Monthly",
-      title: "Enterprise Plan",
-      details: [
-        "Tailored solutions for large enterprises",
-        "Comprehensive features",
-        "Custom development",
-        "Dedicated support",
-        "24/7 phone support",
-        "On-site training sessions",
-        "Service level agreements (SLAs) included",
-      ],
-    },
-  ];
+  const { data, isLoading, error } = useGetAllPackagesQuery();
+
+  if (error)
+    return (
+      <div className="flex items-center justify-center min-h-dvh">
+        An error occurred: {error?.message}
+      </div>
+    );
 
   return (
     <section className="section-wrapper min-h-dvh flex items-center py-10 lg:py-0">
@@ -53,24 +24,31 @@ const Pricings = () => {
           </p>
         </div>
 
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
-          {pricings.map((item) => (
-            <div
-              key={item?.id}
-              className="shadow-md rounded-md border p-5 space-y-5"
-            >
-              <div className="space-y-1">
-                <p>{item?.title}</p>
-                <h5>{item?.price}</h5>
-              </div>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 lg:gap-10">
+          {isLoading
+            ? Array.from({ length: 3 }).map((_, index) => (
+                <div key={index}>
+                  <Skeleton height={50} className="rounded-md mb-1" />
+                  <Skeleton height={200} className="rounded-md" />
+                </div>
+              ))
+            : data?.map((item) => (
+                <div
+                  key={item?._id}
+                  className="shadow-md rounded-md border p-5 space-y-5"
+                >
+                  <div className="space-y-1">
+                    <p>{item?.packageName}</p>
+                    <h5>{item?.price} / Monthly</h5>
+                  </div>
 
-              <div className="space-y-2">
-                {item.details.map((item, index) => (
-                  <p key={index}>{item}</p>
-                ))}
-              </div>
-            </div>
-          ))}
+                  <div className="space-y-2">
+                    {item?.packageDetails?.map((item, index) => (
+                      <p key={index}>{item}</p>
+                    ))}
+                  </div>
+                </div>
+              ))}
         </div>
       </div>
     </section>
--- src/helpers/axios/axiosInstance.js
+++ src/helpers/axios/axiosInstance.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const instance = axios.create();
 instance.defaults.headers.post["Content-Type"] = "application/json";
-instance.defaults.headers["Accept"] = "application/json";
+instance.defaults.headers.post.Accept = "application/json";
 instance.defaults.timeout = 60000;
 
 // Add a request interceptor
